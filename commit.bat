8426df90755a7e281deaf9c6ce29ef4a399a18e1
ikarus512 <ikarus512@yandex.ru>
2020-06-21 10:37:12 +0300

    doc added

--- commit.bat
+++ commit.bat
@@ -10,5 +10,5 @@ if "%*"=="" (
 echo MSG=%MSG%
 
 REM git fetch && git pull && git status
-REM git config user.name ikarus512 && git config user.email ikarus512 && git config core.editor code
+git config user.name ikarus512 && git config user.email ikarus512@yandex.ru && git config core.editor code
 git add . && git commit -m %MSG% && git push
