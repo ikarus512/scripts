db8b122921f05ac6184b02d92c4ee9de09c3124c
ikarus512 <ikarus512@yandex.ru>
2020-10-12 08:44:17 +0300

    g++ include paths

--- languages/c++/g++/show-macro-definitions.sh
+++ languages/c++/g++/show-macro-definitions.sh
@@ -1,1 +1,6 @@
+# Show default macro definitions
 g++ -dM -E -x c++ - < /dev/null > default_macros.$(uname)
+# Show include paths
+g++ -v                         >> default_macros.$(uname)
+# Show tools
+gcc -dumpspecs                 >> default_macros.$(uname)
